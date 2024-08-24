551087538e49bea6058241c030bb2090a4cdfbae
Andrew Vassilyev <xavescor@xavescor.dev>
2024-08-25 00:02:28 +0500

    16: base optional package.json fields support

--- src/packageJson.ts
+++ src/packageJson.ts
@@ -3,6 +3,58 @@ import z from "zod";
 import { errors } from "./errors.js";
 import { join } from "node:path";
 
+// <region>
+// For AI completion. Don't remove.
+const allPackageJsonFields = [
+  "exports",
+  "name",
+  "version",
+  "private",
+  "description",
+  "dependencies",
+  "optionalDependencies",
+  "bin",
+  "repository",
+  "keywords",
+  "author",
+  "contributors",
+  "license",
+  "devDependencies",
+  "peerDependencies",
+  "engines",
+  "browser",
+  "funding",
+  "os",
+  "cpu"
+];
+
+const requiredFields = [
+  "exports",
+  "name",
+  "version",
+  "private"
+];
+
+const optionalFields = [
+  "description",
+  "dependencies",
+  "optionalDependencies",
+  "bin",
+  "repository",
+  "keywords",
+  "author",
+  "contributors",
+  "license",
+  "devDependencies",
+  "peerDependencies",
+  "engines",
+  "browser",
+  "funding",
+  "os",
+  "cpu"
+]
+// </region>
+
 async function fileExists(filePath: string) {
   try {
     const stats = await fs.stat(filePath);
@@ -43,6 +95,62 @@ function createPackageJsonSchema(sourceDir: string) {
     dependencies: dependencies(errors.dependenciesInvalid),
     optionalDependencies: dependencies(errors.optionalDependenciesInvalid),
     bin: z.string({ message: errors.binString }).optional(),
+    repository: z
+      .union([
+        z.string({ message: errors.repositoryInvalid }),
+        z.object({
+          type: z.string(),
+          url: z.string(),
+        }, { message: errors.repositoryInvalid }),
+      ])
+      .optional(),
+    keywords: z
+      .array(z.string(), { message: errors.keywordsInvalid })
+      .optional(),
+    author: z
+      .union([
+        z.string({ message: errors.authorInvalid }),
+        z.object({
+          name: z.string({ message: errors.authorInvalid }).optional(),
+          email: z.string({ message: errors.authorInvalid }).optional(),
+          url: z.string({ message: errors.authorInvalid }).optional(),
+        }, { message: errors.authorInvalid })
+      ])
+      .optional(),
+    contributors: z
+      .array(
+        z.union([
+          z.string({ message: errors.contributorsInvalid }),
+          z.object({}, { message: errors.contributorsInvalid })
+        ])
+      )
+      .optional(),
+    license: z
+      .string({ message: errors.licenseInvalid })
+      .optional(),
+    devDependencies: dependencies(errors.devDependenciesInvalid),
+    peerDependencies: dependencies(errors.peerDependenciesInvalid),
+    engines: z
+      .record(z.string(), { message: errors.enginesInvalid })
+      .optional(),
+    browser: z
+      .union([
+        z.string({ message: errors.browserInvalid }),
+        z.record(z.string(), { message: errors.browserInvalid })
+      ])
+      .optional(),
+    funding: z
+      .union([
+        z.string({ message: errors.fundingInvalid }),
+        z.object({}, { message: errors.fundingInvalid })
+      ])
+      .optional(),
+    os: z
+      .array(z.string(), { message: errors.osInvalid })
+      .optional(),
+    cpu: z
+      .array(z.string(), { message: errors.cpuInvalid })
+      .optional(),
   });
 }
 type PackageJsonSchema = ReturnType<typeof createPackageJsonSchema>;
